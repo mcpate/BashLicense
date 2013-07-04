82a53d41724524f0439ac9d726b5b52fb19e11b2
Matthew Pate <mcpate@gmail.com>
2013-07-03 20:21:59 -0700

    Deletes script.bash.

--- script.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-$textblock="Copyright (c) [year] [name]\nThis program is licensed under the [license]\nPlease see the file COPYING in the source distribution\nof this software for license terms.\n"
-
-for var in "$@"
-do
-    echo -e "$textblock$(cat $var)" > $var
-done
